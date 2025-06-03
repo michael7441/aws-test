044cb45b33f5f0017297125e619d7195d44089f8
Michael Singleton <67748145+michael7441@users.noreply.github.com>
2025-06-03 16:12:11 +0100

    Update ncco3.js

--- ncco3.js
+++ ncco3.js
@@ -9,7 +9,7 @@
         "action": "talk",
         "bargeIn": true,
         "loop": "1",
-        "text": "this is the third text to speech"
+        "text": "this is the third text to speech with guid"
     },
     {
         "action": "input",
@@ -18,6 +18,11 @@
             "maxDigits": "1",
             "timeOut": 10
         },
+        "eventMethod": "GET",
+        "eventUrl":
+        [
+            "https://raw.githubusercontent.com/michael7441/aws-test/refs/heads/main/ncco4.js?michael=e0cf9427-6fc0-4173-9c5e-291a6d6a7326"
+        ],
         "type":
         [
             "dtmf"
