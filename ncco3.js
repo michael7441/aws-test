a66f2336ca54ed67f07a3ee16acac630191a4f41
Michael Singleton <67748145+michael7441@users.noreply.github.com>
2025-06-03 16:27:13 +0100

    Update ncco3.js

--- ncco3.js
+++ ncco3.js
@@ -1,15 +1,9 @@
 [
     {
-        "action": "record",
-        "channels": "2",
-        "format": "wav",
-        "split": "conversation"
-    },
-    {
         "action": "talk",
         "bargeIn": true,
         "loop": "1",
-        "text": "this is the third text to speech with guid"
+        "text": "third message with no record action"
     },
     {
         "action": "input",
