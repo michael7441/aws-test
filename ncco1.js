ca3add8169dc7a6303f4fbdac2a868376b1551b8
Michael Singleton <67748145+michael7441@users.noreply.github.com>
2025-06-03 16:02:25 +0100

    Update ncco1.js

--- ncco1.js
+++ ncco1.js
@@ -1,13 +1,28 @@
 [
     {
         "action": "talk",
-        "text": "one one one"
+        "bargeIn": false,
+        "loop": "1",
+        "text": "this is a long piece of text"
+    },
+    {
+        "action": "record",
+        "channels": "2",
+        "format": "wav",
+        "split": "conversation"
+    },
+    {
+        "action": "talk",
+        "bargeIn": true,
+        "premium": true,
+        "style": "6",
+        "text": "Are you looking near zip code 4 3 1 3 8 ? If this is correct, press 1, otherwise press 2."
     },
     {
         "action": "input",
         "dtmf":
         {
-            "maxDigits": 1,
+            "maxDigits": "1",
             "timeOut": 10
         },
         "eventMethod": "GET",
