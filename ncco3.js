564082c1c3dbab41a9d211f4cce3618fd371ea8b
Michael Singleton <67748145+michael7441@users.noreply.github.com>
2025-06-03 16:02:38 +0100

    Create ncco3.js

---
+++ ncco3.js
@@ -0,0 +1,26 @@
+[
+    {
+        "action": "record",
+        "channels": "2",
+        "format": "wav",
+        "split": "conversation"
+    },
+    {
+        "action": "talk",
+        "bargeIn": true,
+        "loop": "1",
+        "text": "this is the third text to speech"
+    },
+    {
+        "action": "input",
+        "dtmf":
+        {
+            "maxDigits": "1",
+            "timeOut": 10
+        },
+        "type":
+        [
+            "dtmf"
+        ]
+    }
+] 
