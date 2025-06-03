c673f1bdac1f7a1e335771efde493cee2d506f58
Michael Singleton <67748145+michael7441@users.noreply.github.com>
2025-06-03 16:02:12 +0100

    Create ncco2.js

---
+++ ncco2.js
@@ -0,0 +1,25 @@
+[
+    {
+        "action": "talk",
+        "bargeIn": true,
+        "loop": "1",
+        "text": "this is the second text to speech"
+    },
+    {
+        "action": "input",
+        "dtmf":
+        {
+            "maxDigits": "1",
+            "timeOut": 10
+        },
+        "eventMethod": "GET",
+        "eventUrl":
+        [
+            "https://raw.githubusercontent.com/michael7441/aws-test/refs/heads/main/ncco3.js"
+        ],
+        "type":
+        [
+            "dtmf"
+        ]
+    }
+] 
